0f482bbcc5b6fc578ced03a3b1e77d45105687a8
root <fahad2191@gmail.com>
2014-11-25 17:53:07 -0600

    a

--- code/Proj1/XGBT1.py
+++ code/Proj1/XGBT1.py
@@ -1,4 +1,5 @@
 import sys
+import os
 import pandas as pd
 import shutil
 from sklearn.feature_extraction.text import CountVectorizer
@@ -32,11 +33,29 @@ if __name__ == '__main__':
     
     X_train, Y_train, X_test = make_train_test(df_train, df_test)
     
-    dtrain = xgb.DMatrix(X_train, label=Y_train)
+    xg_train = xgb.DMatrix(X_train, label=Y_train)
+    xg_test = xgb.DMatrix(X_test)
     
-    param = {'bst:max_depth':2, 'bst:eta':1, 'silent':1, 'objective':'binary:logistic' }
+    # setup parameters for xgboost
+    param = {}
+    # use softmax multi-class classification
+    param['objective'] = 'multi:softmax'
+    # scale weight of positive examples
+    param['eta'] = 0.1
+    param['max_depth'] = 10
+    param['silent'] = 1
     param['nthread'] = 4
-    plst = param.items()
+    param['num_class'] = 5
     
-    num_round = 10
-    bst = xgb.train( plst, dtrain, num_round)
+    watchlist = [ (xg_train,'train'), (xg_test, 'test') ]
+    num_round = 1000
+    
+    bst = xgb.train( param, xg_train, num_round)    
+    ypred = bst.predict( xg_test )
+    
+    df_output['Sentiment'] = ypred.astype(int)
+    
+    if not os.path.exists(clfFolder):
+        os.makedirs(clfFolder)
+
+    df_output.to_csv(clfFolder + "output.csv", index = False)
